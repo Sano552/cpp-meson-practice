a1b99ca6ddab070f97f0b1901b8b0d8afb2111a9
OneCOR-E <alexjigaloff@yandex.ru>
2022-04-10 15:06:06 +0300

    Add temp

--- temp.cpp
+++ temp.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-cout<<"heloo"<<endl;
+int sguare_side;
+cin>>sguare_side;
+int sguare_P=sguare_side*4;
+int sguare_S=sguare_side*sguare_side;
+cout<<sguare_P<<endl;
+cout<<sguare_S<<endl;
    return 0; 
 }
